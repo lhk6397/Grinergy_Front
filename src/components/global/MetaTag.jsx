f7c0aec57871595735167fbeef1d90bb07219379
maruhxn <a23686397@gmail.com>
2023-06-29 10:54:48 +0900

    metatag 수정

--- src/components/global/MetaTag.jsx
+++ src/components/global/MetaTag.jsx
@@ -19,50 +19,10 @@ const MetaTag = () => {
   }, [pathname]);
   return (
     <Helmet>
-      <meta charset="utf-8" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
       <meta property="og:title" content={title} />
-      <meta property="og:url" content={"https://www.grinergy.tech"} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="/og-image.jpg" />
-      <meta
-        property="og:description"
-        content={"환경, 혁신, 에너지, 그리고 푸른 미래를 향한 그리너지"}
-      />
-      <meta property="og:site_name" content="GRINERGY" />
-      <meta property="og:locale" content="ko_KR" />
-      <meta property="og:locale" content="en_US" />
-      <meta
-        name="twitter:card"
-        content="환경, 혁신, 에너지, 그리고 푸른 미래를 향한 그리너지"
-      />
       <meta name="twitter:title" content={title} />
-      <meta
-        name="twitter:description"
-        content={"환경, 혁신, 에너지, 그리고 푸른 미래를 향한 그리너지"}
-      />
-      <meta name="twitter:image" content="/og-image.jpg" />
       <link rel="canonical" href={`https://www.grinergy.tech${link}`} />
-      <meta
-        name="description"
-        content={"환경, 혁신, 에너지, 그리고 푸른 미래를 향한 그리너지"}
-      />
+
       <title>{title}</title>
     </Helmet>
   );
